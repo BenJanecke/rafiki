088c62f124be0b12210586f7b06093575bbe47e2
Ben Janecke <ben.janecke@peachpayments.com>
2024-10-19 17:07:38 +0200

    Fixes init

--- localenv/stokvel/dbinit.sql
+++ localenv/stokvel/dbinit.sql
@@ -1,11 +1,35 @@
-CREATE USER stokvel_bank_backend WITH PASSWORD 'stokvel_bank_backend';
+CREATE USER cloud_nine_wallet_backend WITH PASSWORD 'cloud_nine_wallet_backend';
 
-CREATE DATABASE stokvel_bank_backend;
+CREATE DATABASE cloud_nine_wallet_backend;
 
-ALTER DATABASE stokvel_bank_backend OWNER TO stokvel_bank_backend;
+ALTER DATABASE cloud_nine_wallet_backend OWNER TO cloud_nine_wallet_backend;
 
-CREATE USER stokvel_bank_auth WITH PASSWORD 'stokvel_bank_auth';
+CREATE USER cloud_nine_wallet_auth WITH PASSWORD 'cloud_nine_wallet_auth';
 
-CREATE DATABASE stokvel_bank_auth;
+CREATE DATABASE cloud_nine_wallet_auth;
 
-ALTER DATABASE stokvel_bank_auth OWNER TO stokvel_bank_auth;
+ALTER DATABASE cloud_nine_wallet_auth OWNER TO cloud_nine_wallet_auth;
+
+CREATE USER happy_life_bank_backend WITH PASSWORD 'happy_life_bank_backend';
+
+CREATE DATABASE happy_life_bank_backend;
+
+ALTER DATABASE happy_life_bank_backend OWNER TO happy_life_bank_backend;
+
+CREATE USER happy_life_bank_auth WITH PASSWORD 'happy_life_bank_auth';
+
+CREATE DATABASE happy_life_bank_auth;
+
+ALTER DATABASE happy_life_bank_auth OWNER TO happy_life_bank_auth;
+
+CREATE USER stokvel_wallet_backend WITH PASSWORD 'stokvel_wallet_backend';
+
+CREATE DATABASE stokvel_wallet_backend;
+
+ALTER DATABASE stokvel_wallet_backend OWNER TO stokvel_wallet_backend;
+
+CREATE USER stokvel_wallet_auth WITH PASSWORD 'stokvel_wallet_auth';
+
+CREATE DATABASE stokvel_wallet_auth;
+
+ALTER DATABASE stokvel_wallet_auth OWNER TO stokvel_wallet_auth;
